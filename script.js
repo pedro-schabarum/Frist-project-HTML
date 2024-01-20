424bf50c5282ba96a75f17656d0fe794cc064f9d
Pedro Guilherme Merisio Schabarum <pedro.guilherme0803@gmail.com>
2024-01-20 20:49:14 -0300

    alteração tag audio

--- script.js
+++ script.js
@@ -2,29 +2,34 @@ document.addEventListener("DOMContentLoaded", function() {
     var audioPlayer = document.getElementById("audioPlayer");
 
     function carregarMusicas() {
-        var musicas = [
-            "mp3/Valsa do pardal orquestra.mp3",
-            "mp3/#Prelude_for_strings.mp3",
-            "mp3/Dream rest.mp3",
-            "mp3/Fantasy in D minor.mp3",
-            "mp3/Flammen.mp3",
-            "mp3/Quarteto de tubas.mp3",
-            "mp3/Scherzo.mp3",
-            "mp3/sintomas de euforia.mp3",
-            "mp3/Sonata nº X.mp3",
-            "mp3/Trio_for_Strings.mp3",
-            "mp3/Valsa do Diabo.mp3",
-            "mp3/Valsa do pardal orquestra.mp3",
-            "mp3/Valsa em Amenor.mp3",
-            "mp3/Valsa em Cmenor.mp3",
-            "mp3/Walk trought the Mountain.mp3"
-        ];
+        var diretorioMusica = "mp3/";
 
-        // Sorteia um índice aleatório para escolher uma música
-        var indiceSorteado = Math.floor(Math.random() * musicas.length);
+        // Lê os arquivos na pasta e carrega uma música aleatória
+        lerArquivos(diretorioMusica, function(musicas) {
+            // Sorteia um índice aleatório para escolher uma música
+            var indiceSorteado = Math.floor(Math.random() * musicas.length);
 
-        // Atualiza o atributo 'src' do elemento de áudio com a música sorteada
-        audioPlayer.src = musicas[indiceSorteado];
+            // Atualiza o atributo 'src' do elemento de áudio com a música sorteada
+            audioPlayer.src = diretorioMusica + musicas[indiceSorteado];
+        });
+    }
+
+    function lerArquivos(diretorio, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    var linhas = xhr.responseText.split(/\r?\n/);
+                    var musicas = linhas.filter(linha => linha.endsWith(".mp3"));
+                    callback(musicas);
+                } else {
+                    console.error("Erro ao carregar as músicas", xhr.status, xhr.statusText);
+                }
+            }
+        };
+
+        xhr.open("GET", diretorio, true);
+        xhr.send();
     }
 
     // Carrega as músicas quando a página é carregada
