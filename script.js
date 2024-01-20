4246f6301a74d37b9c084a6527268a39cd8a06d9
Pedro Guilherme Merisio Schabarum <pedro.guilherme0803@gmail.com>
2024-01-20 20:45:15 -0300

    listagem das músicas

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function carregarMusicas() {
         var musicas = [
-            "mp3/a do sonho.mp3",
-            "mp3/Valsa do pardal orquestra.mp3"
-            // Adicione mais músicas conforme necessário
+            "mp3/Valsa do pardal orquestra.mp3",
+            "mp3/#Prelude_for_strings.mp3",
+            "mp3/Dream rest.mp3",
+            "mp3/Fantasy in D minor.mp3",
+            "mp3/Flammen.mp3",
+            "mp3/Quarteto de tubas.mp3",
+            "mp3/Scherzo.mp3",
+            "mp3/sintomas de euforia.mp3",
+            "mp3/Sonata nº X.mp3",
+            "mp3/Trio_for_Strings.mp3",
+            "mp3/Valsa do Diabo.mp3",
+            "mp3/Valsa do pardal orquestra.mp3",
+            "mp3/Valsa em Amenor.mp3",
+            "mp3/Valsa em Cmenor.mp3",
+            "mp3/Walk trought the Mountain.mp3"
         ];
 
         // Sorteia um índice aleatório para escolher uma música
