4d07beb809d446afc17e774e98533b4165e7932d
Pedro Guilherme Merisio Schabarum <pedro.guilherme0803@gmail.com>
2024-01-25 23:23:46 -0300

    Novo menu catálogo

--- script.js
+++ script.js
@@ -25,3 +25,17 @@ document.addEventListener("DOMContentLoaded", function() {
     // Carrega as músicas quando a página é carregada
     carregarMusicas();
 });
+
+const cardsContainer = document.querySelector(".container");
+
+cardsContainer.addEventListener("click", (e) => {
+  const target = e.target.closest(".card");
+
+  if (!target) return;
+
+  cardsContainer.querySelectorAll(".card").forEach((card) => {
+    card.classList.remove("active");
+  });
+
+  target.classList.add("active");
+});
