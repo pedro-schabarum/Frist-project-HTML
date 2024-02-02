cc6dff357bc39d16517fd989624cb9bd5752cc19
Pedro Guilherme Merisio Schabarum <pedro.guilherme0803@gmail.com>
2024-02-02 12:01:24 -0300

    dev composições

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var audioPlayer = document.getElementById("audioPlayer");
+// document.addEventListener("DOMContentLoaded", function () {
 
-    function carregarMusicas() {
-        var musicas = [
-            "mp3/Valsa do pardal orquestra.mp3",
-            "mp3/Dream rest.mp3",
-            "mp3/Quarteto de tubas.mp3",
-            "mp3/Scherzo.mp3",
-            "mp3/Sonata nº X.mp3",
-            "mp3/Trio_for_Strings.mp3",
-            "mp3/Valsa em Amenor.mp3",
-            "mp3/Valsa em Cmenor.mp3",
-            "mp3/Walk trought the Mountain.mp3",
-            "mp3/Aquele com piano e a harpa.mp3"
-        ];
+//   var audioPlayer = document.getElementById("audioPlayer");
 
-        // Sorteia um índice aleatório para escolher uma música
-        var indiceSorteado = Math.floor(Math.random() * musicas.length);
+//   function carregarMusicas() {
+//     var musicas = [
+//       "mp3/Valsa do pardal orquestra.mp3",
+//       "mp3/Dream rest.mp3",
+//       "mp3/Quarteto de tubas.mp3",
+//       "mp3/Scherzo.mp3",
+//       "mp3/Sonata nº X.mp3",
+//       "mp3/Trio_for_Strings.mp3",
+//       "mp3/Valsa em Amenor.mp3",
+//       "mp3/Valsa em Cmenor.mp3",
+//       "mp3/Walk trought the Mountain.mp3",
+//       "mp3/Aquele com piano e a harpa.mp3"
+//     ];
 
-        // Atualiza o atributo 'src' do elemento de áudio com a música sorteada
-        audioPlayer.src = musicas[indiceSorteado];
-    }
+//     // Sorteia um índice aleatório para escolher uma música
+//     var indiceSorteado = Math.floor(Math.random() * musicas.length);
 
-    // Carrega as músicas quando a página é carregada
-    carregarMusicas();
+//     // Atualiza o atributo 'src' do elemento de áudio com a música sorteada
+//     audioPlayer.src = musicas[indiceSorteado];
+//   }
+  
+//   // Carrega as músicas quando a página é carregada
+//   carregarMusicas();
+// });
+
+document.addEventListener("click", (e) => {
+  const targetCard = e.target.closest(".card");
+  const targetContainer = e.target.closest(".container");
+
+  if (targetCard && targetContainer) {
+    const cardsContainer = targetContainer;
+
+    cardsContainer.querySelectorAll(".card").forEach((card) => {
+      card.classList.remove("active");
+    });
+
+    targetCard.classList.add("active");
+  }
 });
 
-const cardsContainer = document.querySelector(".container");
-
-cardsContainer.addEventListener("click", (e) => {
-  const target = e.target.closest(".card");
-
-  if (!target) return;
-
-  cardsContainer.querySelectorAll(".card").forEach((card) => {
-    card.classList.remove("active");
-  });
-
-  target.classList.add("active");
-});
